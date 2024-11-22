c71dc7fc117758d65f4e3ad50764b215dfab71e9
Łukasz Bacik <mail@luka.sh>
2024-11-22 19:06:41 +0100

    Amendments in FilterCriteria

--- src/JsonHub/Core/FilterCriteria.php
+++ src/JsonHub/Core/FilterCriteria.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace JsonHub\Core;
 
-class FilterCriteria
+use Sushi\ValueObject;
+use Sushi\ValueObject\Invariant;
+
+class FilterCriteria extends ValueObject
 {
     public function __construct(
-        public string | null $searchString = null,
-        public string | null $slug = null,
-        public string | null $definition = null,
-        public string | false | null $parent = null,
-        public string | null $owner = null,
-        public bool | null $private = null,
+        public string|null $searchString = null,
+        public string|null $slug = null,
+        public string|null $definition = null,
+        public string|false|null $parent = null,
+        public string|null $owner = null,
+        public bool|null $private = null,
         public int $offset = 0,
         public int $limit = 10,
     ) {
+        parent::__construct();
+    }
+
+    #[Invariant]
+    public function accessToPrivateItems(): void
+    {
+        if ($this->private && !$this->owner) {
+            throw new \InvalidArgumentException('Owner is required to get private entities');
+        }
     }
 }
